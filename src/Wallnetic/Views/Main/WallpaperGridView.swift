82838efa9cd5fe1e3e9540e49d6d2e4c35c3e311
fatih KAN <fatihkan@hotmail.com.tr>
2026-03-16 09:20:06 +0300

    fix: remove AI case references from WallpaperGridView

--- src/Wallnetic/Views/Main/WallpaperGridView.swift
+++ src/Wallnetic/Views/Main/WallpaperGridView.swift
@@ -25,7 +25,7 @@ struct WallpaperGridView: View {
             wallpapers = wallpapers
                 .filter { $0.dateAdded > oneWeekAgo }
                 .sorted { $0.dateAdded > $1.dateAdded }
-        case .aiGenerate, .aiHistory, .collections, .collection:
+        case .collections, .collection:
             break // Not used in grid view
         }
 
